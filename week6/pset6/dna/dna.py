8fcfcd5a1c39becbed9397eccac6c368c2fa8ead
sina hosseinizad <ssh@ammonit.com>
2020-12-04 17:13:01 +0100

    Also generate where in the sequence each STR is showing and for how long, does not work for 8  and 9. It is also stupid, since I can easily use python built ins like string.count or find or index

--- week6/pset6/dna/dna.py
+++ week6/pset6/dna/dna.py
@@ -7,21 +7,64 @@ import sys
 import csv
 ###===
 
-def sequence_profiler(sequenceFile, srts):
+def updateProfileDict(Prof, count, flag, STR):
+    seq, max = Prof[STR]
+    if count > max:
+        max = count
+    for _ in range(count):
+        seq[flag] = count
+        flag += len(STR)
+    Prof[STR] = seq, max
+
+
+def STR_hunter(seq, flag, STR):
     '''
-    gets the string of the sequnece and a list of strring s of SRTs
-    and marks how far each SRT runs in the sequence
+    Once a `specific` STR is found, the sequence is passed to this function and it 
+    tries to find out how far it goes
+    '''
+    count = 0
+    while flag < len(seq):
+        if seq[flag:flag + len(STR)] == STR:
+            flag += len(STR)
+            count += 1
+        else:
+            flag += 1
+            return count
+    return count
+
+
+def sequence_profiler(sequenceFile, strs):
+    '''
+    opens the sequnece file into a string and a list of string s of STRs
+    and marks how far each STR runs in the sequence
     '''
     # First read the sequnece
     f = open(sequenceFile)
     seq = f.read()
     f.close()
-    # We read this string into a list of tuple with first item the SRT and the second
-    # one its longest run from there on
-    flag = 0 # where we are in the sequence
-    while flag < len(seq):
-        
     
+    # Sequence is profiled in a dictionary with strs as keys and a tuple of (string, max)
+    # as value, string shows where each sequence of str starts and for how long
+    # if str = "AGATC", then 
+    # GGAGATCAGATCAGATCAGATCTAT
+    # 0040000300002000010000
+    profileDict = {}
+    for str in strs:
+        profileDict[str] = ([0] * len(seq), 0)
+
+    flag = 0 # where we are in the sequence
+    count  = 0 # For each STR, after one is found
+    while flag < len(seq):
+        for str in strs:
+            if seq[flag:flag+len(str)] == str:
+                count = STR_hunter(seq, flag, str)
+                updateProfileDict(profileDict, count, flag, str)
+                flag += count * len(str) - 1 # -1 since it gets added a 1 anyway
+                count = 0
+                break
+        flag += 1
+    return profileDict
+
     # profile = dict()
     # for str in strs:
     #     max = count = flag = 0
@@ -48,12 +91,13 @@ def databasereader(csvfile, seqfile):
         # What STRs are of interest
         STRList = rows.fieldnames[1:]
         # Calculate the longest consequetive STRs for the sequence
-        profiledict = sequence_profiler(seqfile, STrList)
+        profiledict = sequence_profiler(seqfile, STRList)
         found = False
         for suspect in rows:
             suspect = dict(suspect)
             for str in STRList:
-                if profiledict[str] == int(suspect[str]):
+                profileseq, max = profiledict[str]
+                if max == int(suspect[str]):
                     found = True
                     continue
                 else:
